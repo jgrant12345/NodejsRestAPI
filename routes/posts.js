64302473ee224c82ab1fd4282a22cbdf936a0b68
Justin Grant <justingrant99@me.com>
2021-01-18 21:01:33 -0800

    added another get functionality

--- routes/posts.js
+++ routes/posts.js
@@ -3,11 +3,9 @@ const express = require('express')
 const router = express.Router();
 const Post = require('../models/post')
 
-router.get('/', (req,res) => {
-    res.send('we are on posts')
-});
 
-router.get('/specific', async (req,res) => {
+// Gets all the posts
+router.get('/', async (req,res) => {
     try {
     const posts = await Post.find();
     res.json(posts)
@@ -16,6 +14,19 @@ router.get('/specific', async (req,res) => {
     }
 });
 
+// Gets a post with a certain title
+router.get('/title/:title', async (req,res) => {
+    try{
+        console.log("hi")
+        const post = await Post.find(
+            {title: "randomPost"}
+        ).limit(2)
+        res.send(post)
+    } catch(err){
+        res.send({messages:err})
+    }
+})
+// SUBMITS A POST
 router.post('/', async (req,res) => {
     const post = new Post({
         title: req.body.title,
@@ -26,10 +37,32 @@ router.post('/', async (req,res) => {
         const savedPost = await post.save();
     res.json(savedPost)
     } catch (err){ 
-        res.json({message: err})
+        res.json({message: err});
 
     }
     
+});
+
+//SPECIFIC POST
+router.get('/:postID', async (req,res) => {
+    try{
+        const post = await Post.findById(req.params.postID)
+        res.json(post);
+    } catch(err) {
+        res.json({message: err});
+    }
+    
+})
+
+//Delete Post
+router.delete('/:postId', async (req, res) => {
+   try{
+    const removedPost = await  Post.remove({_id: req.params.postId})
+    res.json(removedPost)
+   }
+   catch (error){
+    res.json({message:err})
+   }
 });
 
 module.exports = router;
